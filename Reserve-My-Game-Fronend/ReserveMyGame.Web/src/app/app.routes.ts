efdb7fcc618b846c9484d88c9f745b825ec9143d
Rekha Kulkarni <kulkarni.rekha@hotmail.com>
2025-07-01 03:21:26 +0530

    added home,login and user list pages

--- Reserve-My-Game-Fronend/ReserveMyGame.Web/src/app/app.routes.ts
+++ Reserve-My-Game-Fronend/ReserveMyGame.Web/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { UserListComponent } from './pages/user-list/user-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent,children:[{  path:'user-list',component:UserListComponent}] },
+  // Wildcard route for a 404 page (optional)
+  { path: '**', redirectTo: 'login' }
+
+];
