73c92ef785b5ac803134b351f54f10b853c587e8
Rekha Kulkarni <kulkarni.rekha@hotmail.com>
2025-07-02 20:20:47 +0530

    added navbar

--- Reserve-My-Game-Fronend/ReserveMyGame.Web/src/app/pages/home/home.component.ts
+++ Reserve-My-Game-Fronend/ReserveMyGame.Web/src/app/pages/home/home.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink,RouterOutlet],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  router=inject(Router)
+  logOff(){
+    localStorage.removeItem("userApp")
+    this.router.navigateByUrl("Home/Login")
+  }
 
 }
