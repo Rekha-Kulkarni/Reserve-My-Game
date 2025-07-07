7924978fe2f5d9dfa5dd7c768ade9307028628f8
Rekha Kulkarni <kulkarni.rekha@hotmail.com>
2025-07-07 21:43:42 +0530

    aded confirm booking page

--- Reserve-My-Game-Fronend/ReserveMyGame.Web/src/app/app.routes.ts
+++ Reserve-My-Game-Fronend/ReserveMyGame.Web/src/app/app.routes.ts
@@ -4,12 +4,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { UserListComponent } from './pages/user-list/user-list.component';
 import { BookingComponent } from './pages/booking/booking.component';
 import { BookyourgroundComponent } from './pages/bookyourground/bookyourground.component';
+import { ConfirmbookingComponent } from './pages/confirmbooking/confirmbooking.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent,children:[{  path:'user-list',component:UserListComponent}, {  path:'booking',component:BookingComponent},
-    {  path:'bookyourground',component:BookyourgroundComponent}
+    {  path:'bookyourground',component:BookyourgroundComponent}, {  path:'confirmbooking',component:ConfirmbookingComponent}
   ] },
   // Wildcard route for a 404 page (optional)
   { path: '**', redirectTo: 'login' }
--- Reserve-My-Game-Fronend/ReserveMyGame.Web/src/app/pages/confirmbooking/confirmbooking.component.ts
+++ Reserve-My-Game-Fronend/ReserveMyGame.Web/src/app/pages/confirmbooking/confirmbooking.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-confirmbooking',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './confirmbooking.component.html',
   styleUrl: './confirmbooking.component.css'
 })
 export class ConfirmbookingComponent {
 
+constructor(private router: Router) {
+
+  const nav = this.router.getCurrentNavigation();
+ 
 }
+  
+
+  downloadReceipt(): void { alert('Receipt downloaded successfully!'); }
+}
+
+
+
+
+  
